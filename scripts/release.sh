84ed4361fddb516767be918bcb765cf60a4e8ba9
Jeff Lester <44791802+jefflester@users.noreply.github.com>
2023-07-03 19:12:08 -0400

    Update scripts/release.sh
    Co-authored-by: naushadh <naushadh@users.noreply.github.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 DEFAULT_TAG=naushadh/hive-metastore:latest
 TAG=${TAG:-$DEFAULT_TAG}
 
-docker buildx create --name multiarch --use
+docker buildx ls | grep multiarch || docker buildx create --name multiarch --use
 
 docker buildx build --push \
     --platform linux/arm64,linux/amd64 \

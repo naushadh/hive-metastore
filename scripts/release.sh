9f4f3beef460adc6eca1125ced64eb5c5cb5ad5c
Naushadh <naushadh@protonmail.com>
2024-08-21 12:36:28 -0700

    Support releasing tags
    Helps us create Github release, image tag, changelog for each new version (based on git tag).

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,12 +3,14 @@
 # Safeties on: https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euo pipefail
 
-DEFAULT_TAG=naushadh/hive-metastore:latest
-TAG=${TAG:-$DEFAULT_TAG}
+if [ -z ${VERSION+x} ]; then
+    echo "Expected \$VERSION to be defined!";
+    exit 1
+fi
 
 docker buildx ls | grep multiarch || docker buildx create --name multiarch --use
 
 docker buildx build --push \
     --platform linux/arm64,linux/amd64 \
-    --tag "$TAG" \
+    --tag "naushadh/hive-metastore:${VERSION}" \
     .
